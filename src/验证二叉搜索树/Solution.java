183d6021e8c5fa8fe97a2cd5c0c8eff0e9d7bb4a
WTMwand <2389775135@qq.com>
2021-04-20 00:09:35 +0800

    给定一个二叉树，判断其是否是一个有效的二叉搜索树。 假设一个二叉搜索树具有如下特征： 节点的左子树只包含小于当前节点的数。 节点的右子树只包含大于当前节点的数。 所有左子树和右子树自身必须也是二叉搜索树。 示例 1: 输入:     2    / \   1   3 输出: true 示例 2: 输入:     5    / \   1   4      / \     3   6 输出: false

---
+++ src/验证二叉搜索树/Solution.java
@@ -0,0 +1,18 @@
+package 验证二叉搜索树;
+
+
+public class Solution {
+    double last = -Double.MAX_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (isValidBST(root.left)) {
+            if (last < root.val) {
+                last = root.val;
+                return isValidBST(root.right);
+            }
+        }
+        return false;
+    }
+}
