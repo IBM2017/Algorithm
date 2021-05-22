3b53b64b0a780a77e1bcdd0bcfe53a6b9254cc9f
Mr Liu <2389775135@qq.com>
2021-05-22 21:17:01 +0800

    给定一个二叉树 struct Node {   int val;   Node *left;   Node *right;   Node *next; } 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。 初始状态下，所有next 指针都被设置为 NULL。 进阶： 你只能使用常量级额外空间。 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。

---
+++ src/填充每个节点的下一个右侧节点指针2/Solution.java
@@ -0,0 +1,26 @@
+package 填充每个节点的下一个右侧节点指针2;
+
+public class Solution {
+    public Node connect(Node root) {
+        if(root==null) return root;
+        if(root.left!=null && root.right!=null){
+            root.left.next=root.right;
+        }
+        if(root.left!=null && root.right==null){
+            root.left.next=getNext(root.next);
+        }
+        if(root.right!=null)
+            root.right.next=getNext(root.next);
+        connect(root.right);
+        connect(root.left);
+        return root;
+    }
+
+    public Node getNext(Node root){
+        if(root==null) return null;
+        if(root.left!=null) return root.left;
+        if(root.right!=null) return root.right;
+        if(root.next!=null) return getNext(root.next);
+        return null;
+    }
+}
