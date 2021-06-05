b69d9904e7db599716459bf0735756a9bb635445
Mr Liu <2389775135@qq.com>
2021-06-05 13:27:06 +0800

    给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。 说明： 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 示例 1: 输入: [2,2,1] 输出: 1 示例2: 输入: [4,1,2,1,2] 输出: 4

---
+++ src/只出现一次的数字/Solution.java
@@ -0,0 +1,13 @@
+package 只出现一次的数字;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
+        }
+        return single;
+    }
+}
