f5c150fa01a553403152049a3da184279d131d11
Mr Liu <2389775135@qq.com>
2021-06-17 23:45:29 +0800

    峰值元素是指其值大于左右相邻值的元素。 给你一个输入数组nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
    你可以假设nums[-1] = nums[n] = -∞ 。
    
    示例 1：
    
    输入：nums = [1,2,3,1]
    输出：2
    解释：3 是峰值元素，你的函数应该返回其索引 2。
    示例2：
    输入：nums = [1,2,1,3,5,6,4]
    输出：1 或 5
    解释：你的函数可以返回索引 1，其峰值元素为 2；或者返回索引 5， 其峰值元素为 6。

---
+++ src/寻找峰值/Solution.java
@@ -0,0 +1,14 @@
+package 寻找峰值;
+
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        return findPeakElement(nums,0,nums.length-1);
+    }
+    private int findPeakElement(int[] nums,int begin,int end){
+        if(begin == end) return begin;
+        int mid = (begin+end)/2;
+        if(nums[mid]>nums[mid+1])
+            return findPeakElement(nums,begin,mid);
+        return findPeakElement(nums,mid+1,end);
+    }
+}
