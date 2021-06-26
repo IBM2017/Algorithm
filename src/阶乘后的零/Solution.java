ba2d86414493a90bc1f7cd62f9f2ce6c79fbc295
Mr Liu <2389775135@qq.com>
2021-06-26 22:19:20 +0800

    给定一个整数 n，返回 n! 结果尾数中零的数量。
    示例 1:
    输入: 3
    输出: 0
    解释:3! = 6, 尾数中没有零。
    示例2:
    输入: 5
    输出: 1
    解释:5! = 120, 尾数中有 1 个零.

---
+++ src/阶乘后的零/Solution.java
@@ -0,0 +1,12 @@
+package 阶乘后的零;
+
+public class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while(n >= 5) {
+            count += n / 5;
+            n /= 5;
+        }
+        return count;
+    }
+}
